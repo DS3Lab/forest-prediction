179c33becde723051bf6f6420796f64232a10734
lming <lming@student.ethz.ch>
2019-09-01 20:23:40 +0200

    Updated forest cover script

--- semantic_segmentation/unet/data_scraping/web_mercator/final_scripts/forest_cover/preprocess_hansen_cover.py
+++ semantic_segmentation/unet/data_scraping/web_mercator/final_scripts/forest_cover/preprocess_hansen_cover.py
@@ -78,12 +78,13 @@ def process_forest_tile(img_arr):
     no_mask = np.where(img_arr_cp < 0.3) # see how to do no mask
     img_arr[mask] = 1
     img_arr[no_mask] = 0
-    print(np.unique(img_arr))    
+    print(np.unique(img_arr))
     return img_arr
 
 def open_image(path):
     img_arr = cv2.imread(path)
-    return cv2.cvtColor(img_arr, cv2.COLOR_BGR2GRAY)
+    if img_arr is not None:
+        return cv2.cvtColor(img_arr, cv2.COLOR_BGR2GRAY)
 
 def search_hansen_file(name, files):
     for file in files:
@@ -92,48 +93,70 @@ def search_hansen_file(name, files):
             return file
     return None
 
-def create_forest_tile(forest_cover, hansen_loss, out_dir, year, z, x, y):
+def create_forest_tile(forest_cover, hansen_loss, hansen_gain, out_dir, year, z, x, y):
     img_name = 'fc{year}_{z}_{x}_{y}.npy'
     img_name = img_name.format(year=year, z=z, x=x, y=y)
-    mask = np.where(hansen_loss != 0) # set to 0 wherever there is hansen loss
+    loss_mask = np.where(hansen_loss != 0) # set to 0 wherever there is hansen loss
     img_arr = np.copy(forest_cover)
-    img_arr[mask] = 0 # set to 0 where there has been loss
+    if hansen_gain is not None:
+        gain_mask = np.where(hansen_gain != 0) # set to 1 wherever there is hansen gain
+        img_arr[gain_mask] = 1
+    # Since the hansen gain has happened from 2000-2012 and the hansen loss from 2000-2016/18
+    # We assume that the loss has happened after the hansen gain
+    # So in case there is forest loss and forest gain, we assume that there has been
+    # forest loss
+    img_arr[loss_mask] = 0 # set to 0 wherever there has been loss
     np.save(os.path.join(out_dir, img_name), img_arr)
     logger.debug('SUCCESS: {}'.format(img_name))
 
-def create_forest_cover_tiles(forest_files, hansen_files, out_dir):
-    hansen_template = 'ly{year}_{z}_{x}_{y}.png'
+    def create_forest_cover_tiles(forest_files, loss_files, gain_files, out_dir):
+    loss_template = 'ly{year}_{z}_{x}_{y}.png'
+    gain_template = 'fg2012_{z}_{x}_{y}.png'
     for file in forest_files:
         print('processing', file)
         forest_cover = open_image(file)
-        forest_cover = process_forest_tile(forest_cover)
+        forest_cover = process_forest_tile(forest_cover) # >= 0.3 is forest, < 0.3 is not
         year, z, x, y = get_tile_info(file)
-        hansen16 = search_hansen_file(hansen_template.format(year='2016', z=z, x=x, y=y), hansen_files)
-        hansen17 = search_hansen_file(hansen_template.format(year='2017', z=z, x=x, y=y), hansen_files)
-        hansen18 = search_hansen_file(hansen_template.format(year='2018', z=z, x=x, y=y), hansen_files)
+        forest_gain = search_hansen_file
+        hansen16 = search_hansen_file(loss_template.format(year='2016', z=z, x=x, y=y), loss_files)
+        hansen17 = search_hansen_file(loss_template.format(year='2017', z=z, x=x, y=y), loss_files)
+        hansen18 = search_hansen_file(loss_template.format(year='2018', z=z, x=x, y=y), loss_files)
+        gain00 = search_hansen_file(gain_template.format(z=z, x=x, y=y))
+
+        hansen_gain = open_image(gain00)
         if hansen16:
             hansen_loss16 = open_image(hansen16)
-            create_forest_tile(forest_cover, hansen_loss16, out_dir, '2016', z, x, y)
+            create_forest_tile(forest_cover, hansen_loss16, hansen_gain, out_dir, '2016', z, x, y)
         if hansen17:
             hansen_loss17 = open_image(hansen17)
-            create_forest_tile(forest_cover, hansen_loss17, out_dir, '2017', z, x, y)
+            create_forest_tile(forest_cover, hansen_loss17, hansen_gain, out_dir, '2017', z, x, y)
         if hansen18:
             hansen_loss18 = open_image(hansen18)
-            create_forest_tile(forest_cover, hansen_loss18, out_dir, '2018', z, x, y)
+            create_forest_tile(forest_cover, hansen_loss18, hansen_gain, out_dir, '2018', z, x, y)
 
 def main():
     source_dir = '/mnt/ds3lab-scratch/lming/data/min_quality'
-    forest_dir = os.path.join(source_dir, 'forest')
-    out_forest_dir = os.path.join(source_dir, 'forest_cover')
+    forest_cover2000_dir = os.path.join(source_dir, 'forest_cover_raw')
+    forest_gain_2012_dir = os.path.join(source_dir, 'forest_gain')
     hansen_dir = os.path.join(source_dir, 'hansen')
     hansen_other = os.path.join(source_dir, 'hansen_other')
+
+    out_forest_dir = os.path.join(source_dir, 'forest_cover_processed')
     create_dir(out_forest_dir)
-    hansen_files = getListOfFiles(hansen_dir)
-    hansen_files_other = getListOfFiles(hansen_other)
-    hansen_files.extend(hansen_files_other)
+
+    # forest loss
+    forest_loss_files = getListOfFiles(hansen_dir)
+    forest_loss_files_other = getListOfFiles(hansen_other)
+    forest_loss_files.extend(forest_loss_files_other)
+
+    # forest cover
     forest_files = getListOfFiles(forest_dir)
 
-    create_forest_cover_tiles(forest_files, hansen_files, out_forest_dir)
+    # forest gain
+    forest_gain_files = getListOfFiles(forest_gain_2012_dir)
+
+    create_forest_cover_tiles(forest_files, forest_loss_files,
+        forest_gain_files, out_forest_dir)
 
 if __name__ == '__main__':
     main()
