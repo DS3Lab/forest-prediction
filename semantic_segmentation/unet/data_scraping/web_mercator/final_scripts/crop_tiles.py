01fe66c1d317cf7f3fec4964af03344149f39373
lming <lming@student.ethz.ch>
2019-09-02 23:11:32 +0200

    Conflict crop tiles

--- semantic_segmentation/unet/data_scraping/web_mercator/final_scripts/crop_tiles.py
+++ semantic_segmentation/unet/data_scraping/web_mercator/final_scripts/crop_tiles.py
@@ -2,10 +2,17 @@ import shutil
 import os
 import glob
 import image_slicer
+import logging
 
 SRC_PATH = '/mnt/ds3lab-scratch/lming/data/min_quality/planet'
 src_quarter_path = os.path.join(SRC_PATH, 'quarter')
 out_path = os.path.join(SRC_PATH, 'quarter_cropped')
+
+logger = logging.getLogger('crop')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('crop.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 
 def get_prefix(name):
     return name.split('/')[-1][:-4]
@@ -13,9 +20,13 @@ def get_prefix(name):
 def main():
     images = glob.glob(os.path.join(src_quarter_path, '*.png'))
     for image in images:
-        prefix = get_prefix(image)
-        tiles = image_slicer.slice(image, 16, save=False)
-        image_slicer.save_tiles(tiles, directory=out_path, prefix=prefix)
+        try:
+            prefix = get_prefix(image)
+            tiles = image_slicer.slice(image, 16, save=False) # 16 for 64x64 tiles
+            image_slicer.save_tiles(tiles, directory=out_path, prefix=prefix)
+            logger.debug('SUCCESS: ' + image)
+        except:
+            logger.debug('FAILED: ' + image)
 
 if __name__ == '__main__':
     main()
