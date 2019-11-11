d373004ba6d2986e9208a08841045ac1817eee8a
lming <lming@student.ethz.ch>
2019-11-11 21:52:05 +0100

    Clean Project pt 2

--- semantic_segmentation/unet/model/loss.py
+++ semantic_segmentation/unet/model/loss.py
@@ -30,29 +30,6 @@ class LossBinary(nn.Module):
             loss -= self.jaccard_weight * torch.log((intersection + eps) / (union - intersection + eps))
         return loss
 
-class DiceLoss(object):
-    r"""The smoothed Jaccard index loss."""
-
-    def __init__(self, smooth=1.0):
-        self.smooth = smooth
-
-    def __call__(self, outputs, targets):
-        # apply sigmoid
-        outputs = F.sigmoid(outputs)
-
-        # flatten all other dimensions
-        num = outputs.size(0)
-        outputs = outputs.view(num, -1)
-        targets = targets.view(num, -1)
-
-        # apply weights
-        intersection = (outputs * targets).sum(1)
-        scores = 2. * (intersection + self.smooth) / (
-                outputs.sum(1) + targets.sum(1) + self.smooth)
-
-        return 1 - torch.clamp(scores.sum() / num, 0.0, 1.0)
-
-
 def get_jaccard(y_true, y_pred):
     epsilon = 1e-15
     intersection = (y_pred * y_true).sum(dim=-2).sum(dim=-1)
